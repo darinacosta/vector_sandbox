6958dee4fd2c7d134594e2c10c3126c3c3bf8cf0
darinacosta <darinjacosta@gmail.com>
2015-02-19 19:26:59 -0600

    unit vector

--- js/app.js
+++ js/app.js
@@ -1,33 +1,46 @@
 var app = angular.module("vector-land", ['n3-line-chart'])
 
   app.controller("linechart", function($scope){
-    
-    $scope.xValue = 3
-    $scope.yValue = 6
+
+  	$scope.xValue = 10
+    $scope.yValue = 10
+
+  	var calculate2dUnitVector = function(vector){
+  		var hypotenuse = Math.sqrt(Math.pow(vector[0], 2) + Math.pow(vector[1], 2));
+  		vector0 = vector[0]/hypotenuse;
+  		vector1 = vector[1]/hypotenuse;
+  		product = [vector0.toFixed(5), vector1.toFixed(5)];
+  		return product;
+  	};
 
     $scope.$watchCollection('[xValue, yValue]', function(newValues){
       if (isNaN(newValues[0]) === false && isNaN(newValues[1]) === false){
-	      $scope.data = [
-				  {x: 0, value: 0, otherValue: 0},
-				  {x: newValues[0], value: newValues[1], otherValue: 0}
+      	var unitVector = calculate2dUnitVector(newValues);
+      	unitVector[0] !== NaN ? $scope.unitVectorX = unitVector[0] : $scope.unitVectorX = '';
+        unitVector[1] !== NaN ? $scope.unitVectorY = unitVector[1] : $scope.unitVectorX = '';	      
+        $scope.data = [
+				  {x: 0, value: 0, c2: 0},
+				  {x: unitVector[0], value: unitVector[1], c2: newValues[1]/3}
 				];
-				console.log(newValues[1])
+
+				console.log(newValues[1]/10 * 10);
 			}
     })
 
 		$scope.options = {
 		  axes: {
-		    x: {key: 'x', labelFunction: function(value) {return value;}, type: 'linear', min: 0, max: 20, ticks: 2},
-		    y: {type: 'linear', min: 0, max: 20, ticks: 2},
-		    y2: {type: 'linear', min: 0, max: 20, ticks: 2}
+		    x: {key: 'x', labelFunction: function(value) {return value;}, type: 'linear', min: 0, max: 1, ticks: 2},
+		    y: {type: 'linear', min: 0, max: 1, ticks: 2},
+		    y2: {type: 'linear', min: 0, max: 1, ticks: 2}
 		  },
 		  series: [
-		    {y: 'value', color: 'steelblue', thickness: '2px', type: 'linear', striped: true, label: 'Coordinates'},
-		    {y: 'otherValue', axis: 'y2', color: 'lightsteelblue', visible: false, drawDots: true, dotSize: 2}
+		    {y: 'value', color: 'steelblue', thickness: '2px', type: 'linear', striped: true, label: 'Unit Vector'},
 		  ],
 		  lineMode: 'linear',
 		  tension: 0.7,
-		  tooltip: {mode: 'scrubber', formatter: function(x, y, series) {return 'pouet';}},
+		  tooltip: {mode: 'scrubber', formatter: function(x, y, series) {
+		  	return 'x: ' + x + ' y: '+ y;
+		  }},
 		  drawLegend: true,
 		  drawDots: true,
 		  columnsHGap: 5
