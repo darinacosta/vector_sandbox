72b852f16d7e71a04f8058fc6843e2df8a58ab9b
darinacosta <darinjacosta@gmail.com>
2015-02-20 22:24:02 -0600

    template

--- js/app.js
+++ js/app.js
@@ -49,12 +49,16 @@ angular.module("vector-land", ["n3-line-chart","ui.router"])
   }])
 
   .controller("unitVectors", ['$scope', 'vectorFunctions', function($scope, vf){
-    $scope.title = "Unit Vectors"
+    $scope.title = "Calculate Unit Vector"
+    $scope.descrip = "A unit vector is a vector with a length of precisely 1. Input the coordinates of a vector to determine its unit vector equivalent."
+
+
   	$scope.xValue = 10
     $scope.yValue = 10
 
 
     $scope.$watchCollection('[xValue, yValue]', function(newValues){
+      
       if (isNaN(newValues[0]) === false && isNaN(newValues[1]) === false){
       	var unitVector = vf.calculate2dUnitVector(newValues);
       	unitVector[0] !== NaN ? $scope.unitVectorX = unitVector[0] : $scope.unitVectorX = '';
@@ -66,31 +70,31 @@ angular.module("vector-land", ["n3-line-chart","ui.router"])
 
 				console.log(newValues[1]/10 * 10);
 			}
-    })
 
-		$scope.options = {
-		  axes: {
-		    x: {key: 'x', labelFunction: function(value) {return value;}, type: 'linear', min: 0, max: 1, ticks: 2},
-		    y: {type: 'linear', min: 0, max: 1, ticks: 2},
-		    y2: {type: 'linear', min: 0, max: 1, ticks: 2}
-		  },
-		  series: [
-		    {y: 'value', color: 'steelblue', thickness: '2px', type: 'linear', striped: true, label: 'Unit Vector'},
-		  ],
-		  lineMode: 'linear',
-		  tension: 0.7,
-		  tooltip: {mode: 'scrubber', formatter: function(x, y, series) {
-		  	return 'x: ' + x + ' y: '+ y;
-		  }},
-		  drawLegend: true,
-		  drawDots: true,
-		  columnsHGap: 5
-		} 
+			$scope.options = {
+			  axes: {
+			    x: {key: 'x', labelFunction: function(value) {return value;}, type: 'linear', max: 1, ticks: 2},
+			    y: {type: 'linear', max: 1, ticks: 2},
+			    y2: {type: 'linear', max: 1, ticks: 2}
+			  },
+			  series: [
+			    {y: 'value', color: 'steelblue', thickness: '2px', type: 'linear', striped: true, label: 'Unit Vector'},
+			  ],
+			  lineMode: 'linear',
+			  tension: 0.7,
+			  tooltip: {mode: 'scrubber', formatter: function(x, y, series) {
+			  	return 'x: ' + x + ' y: '+ y;
+			  }},
+			  drawLegend: true,
+			  drawDots: true,
+			  columnsHGap: 5
+			} 
+    })
   }])
 
   .controller("hypotenuse", ['$scope', 'vectorFunctions', function($scope, vf){
     
-    $scope.title = "Vector Addition"
+    $scope.title = "Add & Subtract Vectors"
 
   	$scope.x_1 = 3
   	$scope.y_1 = 19
